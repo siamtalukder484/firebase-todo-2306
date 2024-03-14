2f0132f3998def1b1e729b17d61e9cbe7f36644d
Siam Talukder <73644547+siamtalukder484@users.noreply.github.com>
2024-03-14 18:58:41 +0600

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
-import { useState } from 'react'
-import { getDatabase, ref, set ,push,onValue } from "firebase/database";
-
+import { useEffect, useState } from 'react'
+import { getDatabase, ref, set ,push,onValue,remove,update  } from "firebase/database";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
 
   let [text, setText] = useState("")
   let [alltodo, setAlltodo] = useState([])
+  let [isBtn, setIsBtn] = useState(true)
+  let [allinfo, setAllinfo] = useState({})
   const db = getDatabase();
   
   let handleForm = (e) =>{
@@ -18,30 +21,112 @@ function App() {
       set(push(ref(db, 'alltodo')), {
           mytext: text,
       }).then(()=>{
-        console.log("success");
+        toast.success('Todo Created Successfully..', {
+          position: "top-right",
+          autoClose: 2000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: "dark",
+          });
+          setText("")
       });
     }else{
-      console.log("text ny");
+      toast.error('Please Enter some text', {
+        position: "top-right",
+        autoClose: 2000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "light",
+        });
     }
   }
 
   //read operation
+  useEffect(()=>{
+    const todoRef = ref(db, 'alltodo');
+    onValue(todoRef, (snapshot) => {
+      let arr = []
+      snapshot.forEach((item)=>{
+        arr.push({...item.val(), id: item.key})
+      })
+      setAlltodo(arr)
+    });
+  },[])
 
-  const TodoRef = ref(db, 'alltodo');
-onValue(TodoRef, (snapshot) => {
-  let arr = []
-  const data = snapshot.val();
-  snapshot.val().forEach((item)=>{
-    console.log(item);
-  })
-  // console.log(typeof data);
-});
+  // delete operation
+  let handleDelete = (deleteid) => {
+    remove(ref(db, "alltodo/" + deleteid)).then(()=>{
+      toast.error('Delete Successfully...', {
+        position: "top-right",
+        autoClose: 2000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "light",
+        });
+    })
+  }
+
+  // edit operation
+  let handleEdit = (editinfo) => {
+    setIsBtn(false)
+    setText(editinfo.mytext)
+    setAllinfo(editinfo);
+  }
+
+  let handleUpdate = () => {
+    update(ref(db, "alltodo/" + allinfo.id),{
+      ...allinfo,
+      mytext: text, 
+    }).then(()=>{
+      setText("")
+      setIsBtn(true)
+      toast.success('Todo Update Successfully..', {
+        position: "top-right",
+        autoClose: 2000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "dark",
+        });
+    })
+  }
 
 
   return (
     <>
-      <input onChange={handleForm} name='text' placeholder='Enter your task' />
-      <button onClick={handleSubmit} >Submit</button>
+    <ToastContainer/>
+    <div>
+      <h1 className='text-[red] bg-orange-500'>hello tailwind</h1>
+    </div>
+      <input onChange={handleForm} name='text' value={text} placeholder='Enter your task' />
+      {isBtn
+        ?
+        <button onClick={handleSubmit} >Submit</button>
+        :
+        <button onClick={handleUpdate}>Update</button>
+      }
+      <div>
+        <ul className=''>
+          {alltodo.map((item,index)=>(
+            <li key={index}>{item.mytext} 
+            <button onClick={()=>handleDelete(item.id)}>Delete</button>
+            <button onClick={()=>handleEdit(item)}>Edit</button></li>
+          ))
+          }
+          
+        </ul>
+      </div>
     </>
   )
 }
